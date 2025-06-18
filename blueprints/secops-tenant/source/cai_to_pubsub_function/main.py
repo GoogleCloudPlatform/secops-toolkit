1b1fa3a95f45281da5e4352923d1d3fe76079236
bruzzechesse <bruzzechesse@google.com>
2025-06-18 13:00:29 +0400

    fixes

--- blueprints/secops-tenant/source/cai_to_pubsub_function/main.py
+++ blueprints/secops-tenant/source/cai_to_pubsub_function/main.py
@@ -29,7 +29,7 @@ LOGGER = logging.getLogger('cai-chronicle')
 
 
 def generate_timestamp(offset_days=0):
-  """
+    """
   Generates a timestamp string in ISO 8601 format (YYYY-MM-DDTHH:MM:SSZ)
   based on the current UTC time with an optional offset in days.
 
@@ -39,18 +39,22 @@ def generate_timestamp(offset_days=0):
   Returns:
       str: Timestamp string in ISO 8601 format.
   """
-  # Get current UTC time
-  now = datetime.utcnow()
-  # Apply offset
-  timestamp = now + timedelta(days=offset_days)
-  # Format timestamp in ISO 8601 format
-  formatted_time = timestamp.isoformat() + 'Z'
-  return formatted_time
+    # Get current UTC time
+    now = datetime.utcnow()
+    # Apply offset
+    timestamp = now + timedelta(days=offset_days)
+    # Format timestamp in ISO 8601 format
+    formatted_time = timestamp.isoformat() + 'Z'
+    return formatted_time
 
 
-def assets_list(parent, asset_types, content_type, page_size, read_time,
+def assets_list(parent,
+                asset_types,
+                content_type,
+                page_size,
+                read_time,
                 next_page=None):
-  """Fetches assets from Cloud Asset Inventory (CAI) with pagination.
+    """Fetches assets from Cloud Asset Inventory (CAI) with pagination.
 
   Args:
       parent (str): The parent resource name (e.g., projects/your-project-id).
@@ -66,27 +70,28 @@ def assets_list(parent, asset_types, content_type, page_size, read_time,
   Raises:
       Exception: An exception if an error occurs during the request.
   """
-  params = {
-      "assetTypes": asset_types,
-      'contentType': content_type,
-      'pageSize': page_size,
-      'readTime': read_time
-  }
-  if next_page:
-    params['pageToken'] = next_page
+    params = {
+        "assetTypes": asset_types,
+        'contentType': content_type,
+        'pageSize': page_size,
+        'readTime': read_time
+    }
+    if next_page:
+        params['pageToken'] = next_page
 
-  url = 'https://cloudasset.googleapis.com/v1/{parent}/assets'.format(parent=parent)
-  headers = {"Content-Type": "application/json"}
+    url = 'https://cloudasset.googleapis.com/v1/{parent}/assets'.format(
+        parent=parent)
+    headers = {"Content-Type": "application/json"}
 
-  try:
-    response = HTTP.get(url, headers=headers, params=params)
-    return response
-  except Exception as e:
-    raise Exception(f"Error fetching assets: {e}")
+    try:
+        response = HTTP.get(url, headers=headers, params=params)
+        return response
+    except Exception as e:
+        raise Exception(f"Error fetching assets: {e}")
 
 
 def send_to_pubsub(pubsub_topic_id, asset):
-  """
+    """
   Publishes a message to a Pub/Sub.
 
   Args:
@@ -97,23 +102,23 @@ def send_to_pubsub(pubsub_topic_id, asset):
       str: The message ID of the published message (if successful).
       Exception: Raises an exception if there's an error publishing.
   """
-  publisher = pubsub_v1.PublisherClient()
+    publisher = pubsub_v1.PublisherClient()
 
-  try:
-    # Encode message and publish
-    data = json.dumps(asset).encode("utf-8")
-    future = publisher.publish(pubsub_topic_id, data)
-    # Get message ID (success)
-    message_id = future.result()
-    return message_id
-  except Exception as e:
-    # Handle exception (e.g., log error, retry)
-    print(f"Error publishing message to Pub/Sub: {e}")
-    raise  # Re-raise the exception for further handling
+    try:
+        # Encode message and publish
+        data = json.dumps(asset).encode("utf-8")
+        future = publisher.publish(pubsub_topic_id, data)
+        # Get message ID (success)
+        message_id = future.result()
+        return message_id
+    except Exception as e:
+        # Handle exception (e.g., log error, retry)
+        print(f"Error publishing message to Pub/Sub: {e}")
+        raise  # Re-raise the exception for further handling
 
 
 def main(request):
-  """Fetches Cloud Asset Inventory (CAI) assets within a specified lookback period, 
+    """Fetches Cloud Asset Inventory (CAI) assets within a specified lookback period, 
   handles pagination, and sends retrieved assets to Pub/Sub.
 
   This function retrieves assets from CAI based on provided parameters and a 
@@ -124,73 +129,81 @@ def main(request):
   Raises:
       Exception: An exception for critical errors.
   """
-  logging.basicConfig(
-    level=logging.DEBUG if os.environ.get("DEBUG") else logging.INFO,
-    format="[%(levelname)-8s] - %(asctime)s - %(message)s",
-  )
-  logging.root.setLevel(logging.DEBUG)
-  # TODO(): should the lookback period be user configurable
-  lookback_timestamp = generate_timestamp(-1)
+    logging.basicConfig(
+        level=logging.DEBUG if os.environ.get("DEBUG") else logging.INFO,
+        format="[%(levelname)-8s] - %(asctime)s - %(message)s",
+    )
+    logging.root.setLevel(logging.DEBUG)
+    # TODO(): should the lookback period be user configurable
+    lookback_timestamp = generate_timestamp(-1)
 
-  # Expecting values during cloud schedule trigger.
-  request_json = request.get_json(silent=True)
+    # Expecting values during cloud schedule trigger.
+    request_json = request.get_json(silent=True)
 
-  if request_json:
-    nodes = request_json.get("NODES", [])
-    chronicle_assets_config = request_json.get("CHRONICLE_ASSETS_CONFIG")
-    content_type = request_json.get("CONTENT_TYPE")
-    page_size = request_json.get("PAGE_SIZE")
-    pubsub_project_id = request_json.get("PUBSUB_PROJECT_ID")
-  else:
-    LOGGER.error("Did not get configuration parameters from request body.")
-    raise SystemExit('No configuration sent from Cloud Scheduler')
+    if request_json:
+        nodes = request_json.get("NODES", [])
+        chronicle_assets_config = request_json.get("CHRONICLE_ASSETS_CONFIG")
+        content_type = request_json.get("CONTENT_TYPE")
+        page_size = request_json.get("PAGE_SIZE")
+        pubsub_project_id = request_json.get("PUBSUB_PROJECT_ID")
+    else:
+        LOGGER.error("Did not get configuration parameters from request body.")
+        raise SystemExit('No configuration sent from Cloud Scheduler')
 
-  # iterate through all the asset types
-  for chronicle_ingestion_label in chronicle_assets_config.keys():
-    asset_types = chronicle_assets_config[chronicle_ingestion_label][
-        'asset_types']
-    # iterate through all the GCP nodes for tenant
-    for node in nodes:
-      # List to store GCP CAI Assets
-      assets = []
-      more_results = True
-      try:
-        response = assets_list(node, asset_types, content_type, page_size,
-                               lookback_timestamp)
-        while more_results:
-          LOGGER.info(f"response_code: {response.status_code}")
-          if response.status_code == 200:
-            fetched_assets = json.loads(response.text)
-            if "assets" in fetched_assets:
-              for each_asset in fetched_assets["assets"]:  # refactor
-                assets.append(each_asset)
-            if 'nextPageToken' in fetched_assets:
-              LOGGER.info(f"More pages available.")
-              LOGGER.info(f"nextPageToken: {fetched_assets['nextPageToken']}")
-              response = assets_list(node, asset_types, content_type, page_size,
-                                     lookback_timestamp,
-                                     fetched_assets['nextPageToken'])
-            else:
-              if assets:
-                LOGGER.info(f"{len(assets)} assets to be sent to PubSub.")
-                for asset in assets:
-                  send_to_pubsub(
-                      pubsub_topic_id=chronicle_assets_config[
-                          chronicle_ingestion_label]['pubsub_topic_id'],
-                      asset=asset)
-              else:
-                LOGGER.info("No assets returned from GCP CAI.")
-              more_results = False
-          elif response.status_code == 429:
-            LOGGER.info(f"Sleeping for 60 seconds.")
-            time.sleep(60)
-          else:
-            LOGGER.info(f"Catch all for any other HTTP error codes.")
-            more_results = False
-            break
+    # iterate through all the asset types
+    for chronicle_ingestion_label in chronicle_assets_config.keys():
+        asset_types = chronicle_assets_config[chronicle_ingestion_label][
+            'asset_types']
+        # iterate through all the GCP nodes for tenant
+        for node in nodes:
+            # List to store GCP CAI Assets
+            assets = []
+            more_results = True
+            try:
+                response = assets_list(node, asset_types, content_type,
+                                       page_size, lookback_timestamp)
+                while more_results:
+                    LOGGER.info(f"response_code: {response.status_code}")
+                    if response.status_code == 200:
+                        fetched_assets = json.loads(response.text)
+                        if "assets" in fetched_assets:
+                            for each_asset in fetched_assets[
+                                    "assets"]:  # refactor
+                                assets.append(each_asset)
+                        if 'nextPageToken' in fetched_assets:
+                            LOGGER.info(f"More pages available.")
+                            LOGGER.info(
+                                f"nextPageToken: {fetched_assets['nextPageToken']}"
+                            )
+                            response = assets_list(
+                                node, asset_types, content_type, page_size,
+                                lookback_timestamp,
+                                fetched_assets['nextPageToken'])
+                        else:
+                            if assets:
+                                LOGGER.info(
+                                    f"{len(assets)} assets to be sent to PubSub."
+                                )
+                                for asset in assets:
+                                    send_to_pubsub(
+                                        pubsub_topic_id=chronicle_assets_config[
+                                            chronicle_ingestion_label]
+                                        ['pubsub_topic_id'],
+                                        asset=asset)
+                            else:
+                                LOGGER.info("No assets returned from GCP CAI.")
+                            more_results = False
+                    elif response.status_code == 429:
+                        LOGGER.info(f"Sleeping for 60 seconds.")
+                        time.sleep(60)
+                    else:
+                        LOGGER.info(
+                            f"Catch all for any other HTTP error codes.")
+                        more_results = False
+                        break
 
-      except Exception as e:
-        LOGGER.error(f"Unexpected error: {e}")
-        return f'{"status":"500", "data": "{e}"}'
+            except Exception as e:
+                LOGGER.error(f"Unexpected error: {e}")
+                return f'{"status":"500", "data": "{e}"}'
 
-  return '{"status":"200", "data": "OK"}'
+    return '{"status":"200", "data": "OK"}'
--- blueprints/secops-tenant/source/scc_to_secops_function/main.py
+++ blueprints/secops-tenant/source/scc_to_secops_function/main.py
@@ -27,7 +27,7 @@ PROJECT_ID = os.environ.get("PROJECT_ID")
 
 
 def main(req):
-  """Entrypoint.
+    """Entrypoint.
 
   Args:
     req: Request to execute the cloud function.
@@ -35,23 +35,27 @@ def main(req):
   Returns:
     string: "Ingestion completed."
   """
-  client = SecOpsClient()
-  chronicle = client.chronicle(customer_id=SECOPS_CUSTOMER_ID, project_id=PROJECT_ID, region=SECOPS_REGION)
+    client = SecOpsClient()
+    chronicle = client.chronicle(customer_id=SECOPS_CUSTOMER_ID,
+                                 project_id=PROJECT_ID,
+                                 region=SECOPS_REGION)
 
-  # Expecting values from cloud schedule trigger.
-  request_json = req.get_json(silent=True)
+    # Expecting values from cloud schedule trigger.
+    request_json = req.get_json(silent=True)
 
-  if request_json:
-    subscription_id = request_json.get("SUBSCRIPTION_ID")
-    secops_data_type = request_json.get("SECOPS_DATA_TYPE")
-  else:
-    print("Did not get configuration parameters from request body.")
+    if request_json:
+        subscription_id = request_json.get("SUBSCRIPTION_ID")
+        secops_data_type = request_json.get("SECOPS_DATA_TYPE")
+    else:
+        print("Did not get configuration parameters from request body.")
 
-  subscriber = pubsub_v1.SubscriberClient()
-  subscription_path = subscriber.subscription_path(PROJECT_ID, subscription_id)
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(PROJECT_ID,
+                                                     subscription_id)
 
-  def get_and_ingest_messages(message: pubsub_v1.subscriber.message.Message) -> None:
-    """Get message from the subscription.
+    def get_and_ingest_messages(
+            message: pubsub_v1.subscriber.message.Message) -> None:
+        """Get message from the subscription.
 
     Args:
       message: Message received from subscription.
@@ -59,26 +63,27 @@ def main(req):
     Raises:
       ValueError, TypeError: Error when received message is not in json format.
     """
-    print(f"Received {message.data!r}.")
-    message.ack()
-    data = (message.data).decode("utf-8")
-    try:
-      data = json.loads(data)
-    except (ValueError, TypeError) as error:
-      print("ERROR: Unexpected data format received "
-            "while collecting message details from subscription")
-      raise error
+        print(f"Received {message.data!r}.")
+        message.ack()
+        data = (message.data).decode("utf-8")
+        try:
+            data = json.loads(data)
+        except (ValueError, TypeError) as error:
+            print("ERROR: Unexpected data format received "
+                  "while collecting message details from subscription")
+            raise error
 
-    chronicle.ingest_log(log_type=secops_data_type, log_message=json.dumps(data))
+        chronicle.ingest_log(log_type=secops_data_type,
+                             log_message=json.dumps(data))
 
-  future = subscriber.subscribe(subscription_path,
-                                callback=get_and_ingest_messages)
+    future = subscriber.subscribe(subscription_path,
+                                  callback=get_and_ingest_messages)
 
-  with subscriber:
-    try:
-      future.result(timeout=DEFAULT_TIMEOUT)
-    except futures.TimeoutError:
-      future.cancel()
-      future.result()
+    with subscriber:
+        try:
+            future.result(timeout=DEFAULT_TIMEOUT)
+        except futures.TimeoutError:
+            future.cancel()
+            future.result()
 
-  return "Ingestion completed."
+    return "Ingestion completed."
