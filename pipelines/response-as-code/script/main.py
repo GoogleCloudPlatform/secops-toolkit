6e1685b44bd548d3de1063c2d33b6a9eab24cf1f
Simone Bruzzechesse <60114646+simonebruzzechesse@users.noreply.github.com>
2026-04-28 17:09:07 +0200

    New response-as-code pipeline based on Chronicle APIs (#77)
    * New response-as-code pipeline based on Chronicle APIs

--- pipelines/response-as-code/script/main.py
+++ pipelines/response-as-code/script/main.py
@@ -1,10 +1,11 @@
-# Copyright 2025 Google LLC
+#! /usr/bin/env python3
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,62 +13,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from jinja2 import Template
-from soar.git_sync_manager import GitSyncManager
-from soar.constants import PLAYBOOKS_ROOT_README
-import click
-from soar.definitions import Workflow
-from soar.soar_api_client import SiemplifyApiClient
-import os
 import logging
+import os
 import sys
+import time
+import click
+from secops.exceptions import APIError
+from dotenv import load_dotenv
+from models import SocRole, WorkflowMenuCard, Workflow, Template
+from manager import ResponseManager
+from utils import get_local_playbooks, get_local_playbook
+from constants import PLAYBOOKS_ROOT_README
+from config import INCLUDE_BLOCKS, EXCLUDE_FOLDER
 
-LOGGER = logging.getLogger("soar")
-
-# Environment Variables
-INCLUDE_BLOCKS = os.environ.get("INCLUDE_PLAYBOOK_BLOCKS",
-                                "false").lower() == "true"
-EXCLUDE_FOLDER = os.environ.get("EXCLUDE_FOLDER", "Development")
+LOGGER = logging.getLogger("rac")
 
 
-# --- CLI Commands ---
+def create_root_readme() -> str:
+    """
+    Creates the content for the root README file based on the playbooks in the Git repository.
+    :return: The rendered README content as a string.
+    """
+    playbooks = []
+    for pb in get_local_playbooks():
+        playbooks.append(pb.raw_data)
+    readme = Template(PLAYBOOKS_ROOT_README)
+    return readme.render(playbooks=playbooks)
+
+
 @click.group()
 def cli():
     """Manages Response As Code."""
     pass
 
 
-def create_root_readme(gitsync: GitSyncManager):
-    """
-    Creates the content for the root README file based on the playbooks in the Git repository.
-    :param gitsync: The GitSyncManager instance.
-    :return: The rendered README content as a string.
-    """
-    playbooks = []
-    for pb in gitsync.content.get_playbooks():
-        playbooks.append(pb.raw_data)
-    readme = Template(PLAYBOOKS_ROOT_README)
-    return readme.render(playbooks=playbooks)
-
-
 @cli.command(name="sync-playbooks")
 def sync_playbooks():
 
-    api = SiemplifyApiClient(os.environ.get("TARGET_SOAR_API_URL"),
-                             os.environ.get("TARGET_SOAR_API_KEY"), True)
-    soc_roles = api.get_soc_roles()
+    response_manager = ResponseManager()
+    soc_roles: List[SocRole] = response_manager.get_soc_roles()
 
     try:
-        gitsync = GitSyncManager.from_env_vars(soar_api_client=api)
-
         playbooks = {}
-        git_playbooks = list(gitsync.content.get_playbooks())
+        git_playbooks = list(get_local_playbooks())
         LOGGER.info(f"Git Playbooks len {len(git_playbooks)}")
 
         for playbook in git_playbooks:
             LOGGER.info(f"Playbook {playbook.name}")
 
-            overview_templates = playbook.raw_data["overviewTemplates"]
+            overview_templates = playbook.overview_templates
             new_templates = []
             for overview_template in overview_templates:
                 LOGGER.info(f"Roles: {overview_template['roles']}")
@@ -78,9 +72,9 @@ def sync_playbooks():
                             new_roles.append(soc_role["id"])
                 overview_template["roles"] = new_roles
                 new_templates.append(overview_template)
-            playbook.raw_data["overviewTemplates"] = new_templates
+            playbook.overview_templates = new_templates
             LOGGER.info(
-                f"Playbook Updated Overview templates: {playbook.raw_data['overviewTemplates']}"
+                f"Playbook Updated Overview templates: {playbook.overview_templates}"
             )
 
             playbooks[playbook.name] = playbook
@@ -88,11 +82,12 @@ def sync_playbooks():
             if INCLUDE_BLOCKS:
                 for block in playbook.get_involved_blocks():
                     if block.get("name") not in playbooks:
-                        block = gitsync.content.get_playbook(block.get("name"))
+                        block = response_manager.get_playbook(
+                            block.get("name"))
                         if block:
                             playbooks[block.name] = block
 
-        gitsync.install_workflows(list(playbooks.values()))
+        response_manager.install_playbooks(list(playbooks.values()))
 
     except Exception as e:
         LOGGER.error(f"General error performing Sync Playbooks")
@@ -101,74 +96,77 @@ def sync_playbooks():
 
 
 @cli.command(name="pull-playbooks")
-def pull_playbooks():
+@click.pass_obj
+def pull_playbooks(
+    manager_obj: SOARManager
+):  # Renamed manager to manager_obj to avoid conflict with manager instance
     """
     Synchronize SOAR playbooks from the SOAR platform to a Git repository.
     """
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
     commit_msg = os.environ.get("COMMIT_MESSAGE", "Automated playbook sync")
     readme_addon = os.environ.get("README_ADDON")
     include_blocks = os.environ.get("INCLUDE_PLAYBOOK_BLOCKS",
                                     "false").lower() == "true"
 
-    api = SiemplifyApiClient(os.environ.get("SOURCE_SOAR_API_URL"),
-                             os.environ.get("SOURCE_SOAR_API_KEY"), True)
-    soc_roles = api.get_soc_roles()
+    response_manager = ResponseManager()
+
+    soc_roles: List[SocRole] = response_manager.get_soc_roles()
+    print(soc_roles)
 
     try:
-        gitsync = GitSyncManager.from_env_vars(soar_api_client=api)
-        installed_playbooks = gitsync.api.get_playbooks()
+        installed_playbooks: List[
+            WorkflowMenuCard] = response_manager.get_playbooks()
 
         for playbook in installed_playbooks:
-            if EXCLUDE_FOLDER in playbook.get("categoryName"):
+            if EXCLUDE_FOLDER and EXCLUDE_FOLDER in playbook.categoryName:
                 LOGGER.info(
-                    "Skip playbook since belongs to the excluded folder")
+                    f"Skip playbook since belongs to the excluded folder: {playbook.displayName}"
+                )
                 continue
 
-            LOGGER.info(f"Pushing Playbook {playbook['name']}")
+            LOGGER.info(f"Processing Playbook {playbook.displayName}")
 
             if readme_addon:
                 LOGGER.info(
                     "Readme addon found - adding to GitSync metadata file (GitSync.json)"
                 )
                 gitsync.content.metadata.set_readme_addon(
-                    "Playbook", playbook.get("name"), readme_addon)
+                    "Playbook", playbook.displayName, readme_addon)
 
-            playbook = Workflow(
-                gitsync.api.get_playbook(playbook.get("identifier")))
-            overview_templates = playbook.raw_data["overviewTemplates"]
+            playbook_details = Workflow(
+                response_manager.get_playbook(playbook.identifier))
+            LOGGER.info(f"Playbook details: {playbook_details}")
+            overview_templates = playbook_details.overview_templates
             new_templates = []
             for overview_template in overview_templates:
-                LOGGER.info(f"Roles: {overview_template['roles']}")
+                LOGGER.info(f"Roles: {overview_template.get('roles')}")
                 new_roles = []
-                for role in overview_template['roles']:
+                for role in overview_template.get('roles', []):
                     for soc_role in soc_roles:
-                        if soc_role["id"] == role:
-                            new_roles.append(soc_role["name"])
+                        if soc_role.displayName == role:
+                            new_roles.append(soc_role.name)
                 overview_template["roles"] = new_roles
                 new_templates.append(overview_template)
-            playbook.overview_templates = new_templates
+            playbook_details.overview_templates = new_templates
 
-            gitsync.content.push_playbook(playbook)
+            response_manager.store_playbook(playbook_details)
+
             if include_blocks:
-                for block in playbook.get_involved_blocks():
+                for block in playbook_details.get_involved_blocks():
                     installed_block = next(
                         (x for x in installed_playbooks
-                         if x.get("name") == block.get("name")), None)
+                         if x.displayName == block.displayName), None)
                     if not installed_block:
                         LOGGER.warning(
-                            f"Block {block.get('name')} wasn't found in the repo, ignoring"
+                            f"Block {block.displayName} wasn't found in the repo, ignoring"
                         )
                         continue
-                    block = Workflow(
-                        gitsync.api.get_playbook(
-                            installed_block.get("identifier")))
-                    gitsync.content.push_playbook(block)
+                    block_details = Workflow(
+                        response_manager.get_playbook(
+                            installed_block.identifier))
+                    response_manager.store_playbook(block_details)
 
-        gitsync.update_readme(create_root_readme(gitsync), "Playbooks")
+        response_manager.update_readme(create_root_readme(), "Playbooks")
 
     except Exception as e:
         LOGGER.error(f"General error performing Pull Playbooks")
@@ -180,10 +178,8 @@ if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        stream=sys.
-        stdout,  # Explicitly print to stdout for GitHub Actions visibility
+        stream=sys.stdout,
         force=True)
 
-    # Ensure our specific logger is also set to INFO
     LOGGER.setLevel(logging.INFO)
     cli()
