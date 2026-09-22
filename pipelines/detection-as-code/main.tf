0b3730b0d661c598b7454eab4ad331c64813d337
bruzzechesse <bruzzechesse@google.com>
2026-09-22 19:17:51 +0200

    fix issue with dac

--- pipelines/detection-as-code/main.tf
+++ pipelines/detection-as-code/main.tf
@@ -32,41 +32,11 @@ locals {
       }
     ]
   ])
-  reference_lists = try(yamldecode(file("${path.module}/${var.secops_content_config.reference_lists}")), toset([]))
-  reference_list_type_mapping = {
-    STRING = "REFERENCE_LIST_SYNTAX_TYPE_PLAIN_TEXT_STRING"
-    REGEX  = "REFERENCE_LIST_SYNTAX_TYPE_REGEX"
-    CIDR   = "REFERENCE_LIST_SYNTAX_TYPE_CIDR"
-  }
   secops_rules = {
     for file_name in fileset("${path.module}/rules", "*.yaral") : replace(file_name, ".yaral", "") => file("${path.module}/rules/${file_name}")
   }
   secops_rule_deployment = yamldecode(file(var.secops_content_config.rules))
   secops_parent          = "projects/${var.secops_project_id}/locations/${var.secops_region}/instances/${var.secops_customer_id}"
-}
-
-resource "google_chronicle_reference_list" "reference_list" {
-  for_each          = local.reference_lists
-  project           = var.secops_project_id
-  location          = var.secops_region
-  instance          = var.secops_customer_id
-  reference_list_id = each.key
-  description       = each.value.description
-  dynamic "scope_info" {
-    for_each = length(try(each.value.scopes, [])) > 0 ? [""] : []
-    content {
-      reference_list_scope {
-        scope_names = [for scope in each.value.scopes : "${local.secops_parent}/dataAccessScopes/${scope}"]
-      }
-    }
-  }
-  dynamic "entries" {
-    for_each = toset(split("\n", file("${path.module}/reference_lists/${each.key}.txt")))
-    content {
-      value = entries.value
-    }
-  }
-  syntax_type = local.reference_list_type_mapping[each.value.type]
 }
 
 resource "google_chronicle_rule" "rule" {
@@ -78,7 +48,6 @@ resource "google_chronicle_rule" "rule" {
   text            = local.secops_rules[each.key]
   scope           = try("${local.secops_parent}/dataAccessScopes/${each.value.scope}", null)
   depends_on = [
-    google_chronicle_reference_list.reference_list,
     google_chronicle_data_table.data_table
   ]
 }
